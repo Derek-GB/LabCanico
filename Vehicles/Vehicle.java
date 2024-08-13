7972ea3188922f72d80d56d622f8da096fee1093
DylanS.S <dsegurasevilla@gmail.com>
2024-08-13 14:17:22 -0600

    Clase Vehicle y VehicleList
    Se creo la clase Vehicle y VehicleListo con sus respectivos atributos, get,set,buscar,eliminar y agregar

---
+++ Vehicles/Vehicle.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehicles;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author DYLAN
+ */
+public class Vehicle {
+    private String tuition;
+    private String model;
+    private String brand;
+    private LocalDate yearManufacture;
+
+    public String getTuition() {
+        return tuition;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public LocalDate getYearManufacture() {
+        return yearManufacture;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Vehicle(String tuition, String model, String brand, LocalDate yearManufacture) {
+        this.tuition = tuition;
+        this.model = model;
+        this.brand = brand;
+        this.yearManufacture = yearManufacture;
+    }
+
+    public Vehicle() {
+        this("","","",LocalDate.now());
+    }
+    
+    
+    
+}
---
+++ Vehicles/VehicleList.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehicles;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DYLAN
+ */
+public class VehicleList {
+    
+    private ArrayList<Vehicle> vehicles;
+    
+    public VehicleList() {
+        vehicles = new ArrayList<>();
+    }
+    
+    public void addVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+    }
+    
+    public void removeVehicle(String tuition) {
+        vehicles.removeIf(veh -> veh.getTuition() == tuition);
+    }
+    
+    public Vehicle findVehicleByTuition(String tuition) {
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getTuition().equals(tuition)) {
+                return vehicle;
+            }
+        }
+        return null;
+    }
+    
+}
